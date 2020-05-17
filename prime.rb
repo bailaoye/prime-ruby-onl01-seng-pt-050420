0a30cec7cfd0f43cab6bf30d7492ed48465edecc
Jeff Bai <j.bai@wustl.edu>
2020-05-17 01:02:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
-def prime?(number)
+def prime?(input)
   start = 2
-	if number > 1
+	if num > 1
 	  range = (start..number-1).to_a
-	  range.none? do |num_to_test|
-	  number % num_to_test == 0
+	  range.none? do |num|
+	  input % num == 0
 end
      else false
 end
