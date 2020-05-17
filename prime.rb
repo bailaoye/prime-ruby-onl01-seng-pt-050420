732404ca704b9d4baaf2766f26140821fad5593c
Jeff Bai <j.bai@wustl.edu>
2020-05-17 01:04:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,23 +3,8 @@ def prime?(input)
 	if input > 1
 	  range = (start..input - 1).to_a
 	  range.none? do |num|
-	  input % num == 0
-end
-     else false
-end
-end
-
-def prime?(number)
-
-     start = 2
-		 
-		 if number > 1
-		 
-		 range = (start..number-1).to_a
-		 range.none? do |num_to_test|
-		 number % num_to_test == 0
-end
-     else false
-		 
-end
+	    input % num == 0
+    end
+  else false
+  end
 end
